ee9e210bb734fa5ed2acf16b88c919de6658792a
Dheeraj-Sharma07 <dheerajsharma.15072001@gmail.com>
2024-05-21 20:41:41 +0530

    final version from dheeraj side

--- Screens/Explore/Explore.jsx
+++ Screens/Explore/Explore.jsx
@@ -1,26 +1,58 @@
-import React from 'react'
-import './Explore.css'
-import Explore_image from '../../assets/Explore.png'
-import Carousel from '../../components/Carousel/Carousel'
-// const images = [
-//   {Explore_image},
-//   'image2.jpg',
-//   'image3.jpg',
-//   // Add more image URLs as needed
-// ];
+import React, { useState } from 'react';
+import './Explore.css';
+import sliderimg1 from '../../assets/Rectangle70.png';
+import sliderimg2 from '../../assets/Rectangle71.png';
+import sliderimg3 from '../../assets/Rectangle72.png';
+
+const slides = [sliderimg1, sliderimg2, sliderimg3, sliderimg1, sliderimg2];
+
 const Explore = () => {
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
     <div className='explore-main'>
-        <div className='div-left'>
-            <p className='explore-heading'>Explore Our Full Collection</p>
-            <p className='explore-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, eos quisquam recusandae incidunt amet laboriosam sit nostrum ex maxime vitae. Asperiores modi sit aliquam praesentium nesciunt culpa ab aut vero!</p>
-            <button className='explore-btn'> Start Exploring!</button>
+      <div className='div-left'>
+        <p className='explore-heading'>Explore Our Full Collection</p>
+        <p className='explore-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, eos quisquam recusandae incidunt amet laboriosam sit nostrum ex maxime vitae. Asperiores modi sit aliquam praesentium nesciunt culpa ab aut vero!</p>
+        <button className='explore-btn'>Start Exploring!</button>
+      </div>
+      <div className='div-right'>
+        <div className="slider-container">
+          <button className="slider-button"  onClick={handlePrev}>
+            &lt;
+          </button>
+          <div className="slider-wrapper">
+            <div
+              className="slider"
+              style={{ transform: `translateX(-${currentIndex * (100 / 3)}%)` }}
+            >
+              {slides.map((slide, index) => (
+                <div key={index} className="slide">
+                  <img src={slide} alt={`Slide ${index + 1}`} />
+                </div>
+              ))}
+            </div>
+          </div>
+          <button className="slider-button" onClick={handleNext}>
+            &gt;
+          </button>
         </div>
-        <div className='div-right'>
-          <img src={Explore_image} alt="" />
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Explore
+export default Explore;
--- Screens/Navbar/Navbar.jsx
+++ Screens/Navbar/Navbar.jsx
@@ -1,22 +1,69 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom'
-import Collection from '../Collection/Collection'
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+// import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
 const Navbar = () => {
+  const [collectionDropdown, setCollectionDropdown] = useState(false);
+  const [offersDropdown, setOffersDropdown] = useState(false);
+
+  const handleCollectionMouseEnter = () => {
+    setCollectionDropdown(true);
+  };
+
+  const handleCollectionMouseLeave = () => {
+    setCollectionDropdown(false);
+  };
+
+  const handleOffersMouseEnter = () => {
+    setOffersDropdown(true);
+  };
+
+  const handleOffersMouseLeave = () => {
+    setOffersDropdown(false);
+  };
+
   return (
     <div className='navbar'>
-    <img src={logo} alt="logo" className='img'/>
-      <ul  className="nav-menu">
+      <img src={logo} alt="logo" className='img'/>
+      <ul className="nav-menu">
         <li>Home</li>
-        <li>Collection</li>
-        <li>Our Offers</li>
+        <li 
+          className="dropdown" 
+          onMouseEnter={handleCollectionMouseEnter} 
+          onMouseLeave={handleCollectionMouseLeave}
+        >
+          Collection <FontAwesomeIcon icon={faChevronDown} />
+          {collectionDropdown && (
+            <ul className="dropdown-menu">
+              <li>Category 1</li>
+              <li>Category 2</li>
+              <li>Category 3</li>
+            </ul>
+          )}
+        </li>
+        <li
+          className="dropdown" 
+          onMouseEnter={handleOffersMouseEnter} 
+          onMouseLeave={handleOffersMouseLeave}
+        >
+          Our Offers <FontAwesomeIcon icon={faChevronDown} />
+          {offersDropdown && (
+            <ul className="dropdown-menu">
+              <li>Category 1</li>
+              <li>Category 2</li>
+              <li>Category 3</li>
+            </ul>
+          )}
+        </li>
         <li>Our Stories</li>
         <li>Contact Us</li>
         <button className='btn'>Join Us!</button>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
